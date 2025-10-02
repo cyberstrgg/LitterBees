6bc10b2849d3bb30b24b15a6148b0c8e31e96ee2
cyberstrgg <asaraino@gmail.com>
2025-10-02 12:01:19 -0400

    Panning the hive menu

---
+++ RoomView.gd
@@ -0,0 +1,12 @@
+extends Control
+
+@onready var grid_container: GridContainer = $GridContainer
+var is_panning: bool = false
+
+# This function is automatically called when a mouse event happens inside this control.
+func _gui_input(event: InputEvent):
+    if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+        is_panning = event.is_pressed()
+    
+    if event is InputEventMouseMotion and is_panning:
+        grid_container.position += event.relative
--- upgrades_menu.gd
+++ upgrades_menu.gd
@@ -2,15 +2,12 @@ extends CanvasLayer
 
 signal menu_closed
 
-# Update the paths to include the new RoomView node
-@onready var room_view: Control = $CenterContainer/PanelContainer/VBoxContainer/RoomView
+# The reference to room_view is no longer needed here
 @onready var grid: GridContainer = $CenterContainer/PanelContainer/VBoxContainer/RoomView/GridContainer
 @onready var back_button: Button = $CenterContainer/PanelContainer/VBoxContainer/BackButton
 @onready var scrap_label: Label = $CenterContainer/PanelContainer/VBoxContainer/ScrapLabel
 
 const ROOM_SLOT_SCENE = preload("res://room_slot.tscn")
-
-var is_panning: bool = false
 
 func _ready():
     process_mode = Node.PROCESS_MODE_ALWAYS
@@ -22,21 +19,6 @@ func populate_grid():
     for i in range(9):
         var slot = ROOM_SLOT_SCENE.instantiate()
         grid.add_child(slot)
-
-func _unhandled_input(event: InputEvent):
-    # Check if the mouse is inside our RoomView's rectangle
-    var mouse_in_view = room_view.get_global_rect().has_point(event.position)
-    
-    if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-        # Only start panning if the click is inside the view
-        if mouse_in_view:
-            is_panning = event.is_pressed()
-        else:
-            is_panning = false
-
-    if is_panning and event is InputEventMouseMotion:
-        # Move the grid's position
-        grid.position += event.relative
 
 func _on_back_button_pressed():
     queue_free()
